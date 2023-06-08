c0d2d3798c0ad9832e97bd0b4d41d3caa76966a7
colehanekom <38474090+colehanekom@users.noreply.github.com>
2023-06-08 14:16:31 +0200

    Add files via upload

---
+++ Day 1/Dog.java
@@ -0,0 +1,10 @@
+public class Dog {
+    public void bark() {
+        System.out.println("Woof! Woof!");
+    }
+
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.bark();
+    }
+}
---
+++ Day 1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    /*This is my first Java program
+     * This will print 'Hello World as the ouput'
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); //This prints Hello World
+    }
+}
